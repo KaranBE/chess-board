1684acd3c137a3bc06fd9ff6ee171aff1e212970
Karan <karan.bhansali42@gmail.com>
2023-07-11 20:45:25 +0530

    chess-board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const n = 8, m = 8;
+
+  const [chessBoard, setChessBoard] = useState([])
+
+  useEffect(() => {
+    const result = []
+
+    for (let i = 0; i < n; i++) {
+      const row = Array.from({ length: m });
+
+      result.push(row);
+    }
+    setChessBoard(result);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {
+        chessBoard.length > 0 &&
+        chessBoard.map((row, rIndex) => {
+          return (
+            <div key={rIndex} className="row">
+              {row.map((_, cIndex) => {
+                return (
+                  <div key={cIndex} className={`box ${(rIndex + cIndex) % 2 === 0 ? "black" : "white"}`}></div>)
+              })}
+            </div>
+          )
+        })}
+    </div>);
 }
 
 export default App;
